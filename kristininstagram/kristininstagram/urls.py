98164f9cceb91ec8d6d652e25f65a0f04e5b1032
Kim-Link <shkim@kristinkorea.com>
2023-02-15 10:06:05 +0900

    instagram app 생성

--- kristininstagram/kristininstagram/urls.py
+++ kristininstagram/kristininstagram/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include('instagram.urls')),
 ]
 
 if settings.DEBUG:
